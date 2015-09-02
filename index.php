a379d3df40a52388bc5698e0afd3943f1d547b22
gomette <luftlaura@gmail.com>
2015-09-02 14:59:25 -0400

    correction et initialisation a 102

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
 require_once 'data/_data.php';
 $emp_id = 102; // Employé "par défaut"
+if (array_key_exists(PARAM_EMP_ID, $_GET)){
+	$emp_id = $_GET[PARAM_EMP_ID];;
+}
 // TODO: Lire et mettre à jour l'id de l'employé en Query string
 $liste_employes = get_employes(); // Liste des noms de tous les employés
 $emp_data = $liste_employes[$emp_id];
@@ -12,22 +15,21 @@ $agenda = get_agenda($emp_id);
         <title>Agenda Cabinet Médical blah</title>
 	</head>
 <body>
-    <?php require_once('view_bloc/_view_header.php') ?>
-    <h2>Activité de l'employé <?php echo $emp_data['emp_name'] ?> </h2>
-	<div id="agenda">
-		<table>
-			<tr><th>Heure</th><th>Activité</th></tr>
+<?php require_once('view_bloc/_view_header.php') ?>
+<h2>Activité de l'employé <?php echo $emp_data['emp_name'] ?> </h2>
+<div id="agenda">
+	<table>
+		<tr><th>Heure</th><th>Activité</th></tr>
 
-<?php
-
-foreach (get_agenda($_GET[PARAM_EMP_ID]) as $key => $value){
-	echo '<tr><th>',$key,'</th><th>',$value,'</th></tr>';
-}
-// TODO: Afficher l'agenda de l'employé
-if (array_key_exists(PARAM_EMP_ID,$_GET)) {
-	echo $_GET[PARAM_EMP_ID];
-}
-?>
+		<?php
+		foreach (get_agenda($emp_id) as $key => $value){
+			echo '<tr><th>',$key,'</th><th>',$value,'</th></tr>';
+		}
+		// TODO: Afficher l'agenda de l'employé
+		if (array_key_exists(PARAM_EMP_ID,$_GET)) {
+			echo $_GET[PARAM_EMP_ID];
+		}
+		?>
 		</table>
 	</div>
     <?php require_once('view_bloc/_view_footer.php') ?>
