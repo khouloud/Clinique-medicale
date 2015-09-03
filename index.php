ae2ccc4bc80c1175a03d54d71172d95c6b0b59d6
KOKO <khouloudbenmim@gmail.com>
2015-09-03 13:56:24 -0400

    nouveau fishier

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
-<?php 
-require_once 'data/_data.php';
+<?php
+/* inclure le ficiers _data.php */
+require_once __DIR__.'/data/_data.php';
+
 $emp_id = 102; // Employé "par défaut"
 if (array_key_exists(PARAM_EMP_ID, $_GET)){
 	$emp_id = $_GET[PARAM_EMP_ID];;
 }
 // TODO: Lire et mettre à jour l'id de l'employé en Query string
-$liste_employes = get_employes(); // Liste des noms de tous les employés
+$liste_employes = get_employes();
 $emp_data = $liste_employes[$emp_id];
 $agenda = get_agenda($emp_id);
 ?>
 <html>
-	<head>
-        <meta charset = "UTF-8" />
-        <title>Agenda Cabinet Médical blah</title>
-	</head>
+<head>
+	<meta charset = "UTF-8" />
+	<title>Agenda Cabinet Médical blah</title>
+</head>
 <body>
 <?php require_once('view_bloc/_view_header.php') ?>
 <h2>Activité de l'employé <?php echo $emp_data['emp_name'] ?> </h2>
 <div id="agenda">
+	<!--le lien pour vouloir modifier l'agenda d'un employee-->
+	<div><a href="emp_edit.php?<?= PARAM_EMP_ID . '=' . $emp_id ?>">
+			<img border="0" alt="edit" src="pictures/edit.png">
+		</a></div>
 	<table>
 		<tr><th>Heure</th><th>Activité</th></tr>
 
@@ -30,8 +36,9 @@ $agenda = get_agenda($emp_id);
 			echo $_GET[PARAM_EMP_ID];
 		}
 		?>
-		</table>
-	</div>
-    <?php require_once('view_bloc/_view_footer.php') ?>
+
+	</table>
+</div>
+<?php require_once('view_bloc/_view_footer.php') ?>
 </body>
 </html>
